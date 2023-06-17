e9196a2f7f3341f62827d3810d350735129a4abb
TNTChinaAAB <122889923+TNTChinaAAB@users.noreply.github.com>
2023-06-17 08:56:35 -0600

    Update WeghtReader.py

--- WeightReader.py
+++ WeightReader.py
@@ -11,8 +11,9 @@ def readWeightVersion():
     GPU_NAME: str = torch.cuda.get_device_name(0)
     ID: str = hashlib.sha256(GPU_NAME.encode()).hexdigest()[0:8]
     NUM_THREADS = str(Values.numberThreads)
+    tuneSalt = 3
     with gzip.open("/content/work/data/weights/40b.bin.gz") as _weight_file_:
         Values.MODEL_NAME = str(_weight_file_.readlines()[0]).replace("b'", "").replace("\\n'", "")
-    Values.CACHE_NAME = f"trt-{NV_VERSION}_gpu-{ID}_net-{Values.MODEL_NAME}_exact19x19_batch{NUM_THREADS}_fp16"
+    Values.CACHE_NAME = f"trt-{NV_VERSION}_gpu-{ID}_net-{Values.MODEL_NAME}_{tuneSalt}_exact19x19_batch{NUM_THREADS}_fp16"
 
 
